99a8f023259299a3a71a95c8152947964a6fdf9b
Samuel Beddow <beddo018@umn.edu>
2021-12-23 18:38:10 -0600

    collectively installed dependencies, setup server, setup webpack

---
+++ client/src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h1> Hello World </h1>
+      </div>
+    )
+  }
+}
+
+
+export default App;
