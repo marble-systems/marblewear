73f6a0f282ec497b417be8aa0891c9c2e693e636
Regina Grogan <regina.r.grogan@gmail.com>
2021-12-28 11:30:10 -0600

    Change RelatedItems component to pass dummy data to RelatedProducts and YourOutfit components

--- client/src/components/RelatedItems/RelatedItems.jsx
+++ client/src/components/RelatedItems/RelatedItems.jsx
@@ -3,11 +3,17 @@ import React from 'react';
 import RelatedProducts from './RelatedProducts.jsx';
 import YourOutfit from './YourOutfit.jsx';
 
-function RelatedItems(props) {
+function RelatedItems({relatedItemsdata}) {
+  const {
+    currentProductInfo,
+    relatedProducts,
+    relatedProductsInfo,
+    outfitProductInfo
+  } = relatedItemsdata;
   return (
     <div>
-    <RelatedProducts />
-    <YourOutfit />
+      <RelatedProducts currentProductInfo={currentProductInfo} relatedProductsInfo={relatedProductsInfo}/>
+      <YourOutfit outfitProductInfo={outfitProductInfo}/>
     </div>
   )
 }
