50cf64c50d8ab2dbe62d0ae3ab214d5bf0bc9dfd
Regina Grogan <regina.r.grogan@gmail.com>
2021-12-28 11:58:08 -0600

    Change ProductCard to render product info. Some elements in this component are not being implemented yet. Leaving a placeholder for them.

--- client/src/components/RelatedItems/ProductCard.jsx
+++ client/src/components/RelatedItems/ProductCard.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-function ProductCard(props) {
+function ProductCard({relatedProductInfo}) {
   return (
     <div>
-    ProductCard
+      <p>--- Image ---</p>
+      <p>--- Action Button ---</p>
+      <p>{relatedProductInfo.category}</p>
+      <h4>{relatedProductInfo.name}</h4>
+      <p>{relatedProductInfo['default_price']}</p>
+      <p>*** Star Ratings ***</p>
     </div>
   )
 }
