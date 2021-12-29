d3e168dddc5f50c33d59cf4a5d7282f752f66195
Aaron Finsrud <aaronfinsrud@gmail.com>
2021-12-29 15:56:21 -0500

    add sample data for Ratings and Reviews component

--- client/src/components/Reviews/reviewsData.js
+++ client/src/components/Reviews/reviewsData.js
@@ -1,3 +1,100 @@
 module.exports = {
-
-}
+  // GET /reviews/
+  reviews: {
+    'product': '39337',
+    'page': 0,
+    'count': 5,
+    'results': [
+      {
+        'review_id': 623869,
+        'rating': 4,
+        'summary': 'Great shoes!',
+        'recommend': true,
+        'response': '',
+        'body': 'Now I can get to stomping!',
+        'date': '2019-05-04T00:00:00.000Z',
+        'reviewer_name': 'chingy',
+        'helpfulness': 12,
+        'photos': []
+      },
+      {
+        'review_id': 623870,
+        'rating': 3,
+        'summary': 'They\'re heavy but great',
+        'recommend': true,
+        'response': '',
+        'body': 'I like them but they run wide.',
+        'date': '2019-04-13T00:00:00.000Z',
+        'reviewer_name': 'thinfootjim',
+        'helpfulness': 3,
+        'photos': []
+      },
+      {
+        'review_id': 1094837,
+        'rating': 5,
+        'summary': 'yes',
+        'recommend': true,
+        'response': null,
+        'body': 'yes',
+        'date': '2021-11-13T00:00:00.000Z',
+        'reviewer_name': 'hello',
+        'helpfulness': 0,
+        'photos': []
+      },
+      {
+        'review_id': 1094836,
+        'rating': 5,
+        'summary': 'great',
+        'recommend': true,
+        'response': null,
+        'body': 'love it',
+        'date': '2021-11-13T00:00:00.000Z',
+        'reviewer_name': 'hey',
+        'helpfulness': 0,
+        'photos': []
+      },
+      {
+        'review_id': 1094865,
+        'rating': 4,
+        'summary': 'great',
+        'recommend': true,
+        'response': null,
+        'body': 'love it',
+        'date': '2021-11-13T00:00:00.000Z',
+        'reviewer_name': 'jackie',
+        'helpfulness': 0,
+        'photos': []
+      }
+    ]
+  },
+  // GET /reviews/meta
+  reviewsMetadata: {
+    'product_id': '39337',
+    'ratings': {
+      '3': '1',
+      '4': '2',
+      '5': '2'
+    },
+    'recommended': {
+      'true': '5'
+    },
+    'characteristics': {
+      'Size': {
+        'id': 131851,
+        'value': '4.2000000000000000'
+      },
+      'Width': {
+        'id': 131852,
+        'value': '4.0000000000000000'
+      },
+      'Comfort': {
+        'id': 131853,
+        'value': '4.6000000000000000'
+      },
+      'Quality': {
+        'id': 131854,
+        'value': '4.4000000000000000'
+      }
+    }
+  }
+};
