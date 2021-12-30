26e17ebb60cf13bb471cf4d781a5182faa9f86a7
Regina Grogan <regina.r.grogan@gmail.com>
2021-12-29 21:34:55 -0600

    Fix linter errors

--- client/src/components/RelatedItems/ProductCard.jsx
+++ client/src/components/RelatedItems/ProductCard.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ProductCard({relatedProductInfo}) {
   return (
-    <div class='card' style={{width: '20em'}}>
-      <img class="card-img-top" src="..." alt="Card image"></img>
+    <div className="card" style={{width: '20em'}}>
+      <img className="card-img-top" src="..." alt="Card image"></img>
       <p>--- Action Button ---</p>
-      <div class="card-body">
-        <p class="card-text">{relatedProductInfo.category}</p>
-        <h5 class="card-title">{relatedProductInfo.name}</h5>
-        <p class="card-text">{relatedProductInfo.default_price}</p>
+      <div className="card-body">
+        <p className="card-text">{relatedProductInfo.category}</p>
+        <h5 className="card-title">{relatedProductInfo.name}</h5>
+        <p className="card-text">{relatedProductInfo.default_price}</p>
         <p>*** Star Ratings ***</p>
       </div>
     </div>
-  )
+  );
 }
+
+ProductCard.propTypes = {
+  relatedProductInfo: PropTypes.object
+};
 
 export default ProductCard;
