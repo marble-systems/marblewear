c5b610dc6e76a65c175b549fa5d0f4c4963ac64f
Samuel Beddow <beddo018@umn.edu>
2021-12-31 12:45:09 -0600

    Added comments to not-yet-implemented portions of current PR (button, handleChange functions); deleted console.log

--- client/src/components/QandA/QuestionList.jsx
+++ client/src/components/QandA/QuestionList.jsx
@@ -4,7 +4,6 @@ import QuestionListEntry from './QuestionListEntry.jsx';
 import AddQuestion from './AddQuestion.jsx';
 
 const QuestionList = (props) => {
-  console.log(props);
   return (
     <div className="container">
       <p className="text-start fs-5 fw-light">
@@ -12,7 +11,8 @@ const QuestionList = (props) => {
       </p>
       <div className="input-group mb-3">
         <input type="text" className="form-control" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." aria-label="Recipient's username" aria-describedby="button-addon2"></input>
-        <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+        <button className="btn btn-outline-secondary" type="button" id="button-addon2">Filter Questions</button>
+        {/* Above button label will be replaced by a magnifying glass icon in later update FIX ME ^^ */}
       </div>
       <div className="row">
         {props.data.results.map((question, index) => {
