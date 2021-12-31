fa2274e08bb4cca611fc5b1d4045ffc2c2da92a7
Regina Grogan <regina.r.grogan@gmail.com>
2021-12-30 21:55:22 -0600

    Add css file to disable next and previous buttons on carousel. Install style-loader to render css file with webpack. Import css file to components.

--- client/src/components/RelatedItems/RelatedProducts.jsx
+++ client/src/components/RelatedItems/RelatedProducts.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ProductCard from './ProductCard.jsx';
 import ComparisonModal from './ComparisonModal.jsx';
+import './relatedItemsStyle.css';
 
 function RelatedProducts({currentProductInfo, relatedProductsInfo}) {
   return (
     <div>
-      <div className="main-gallery js-flickity" data-flickity='{"pageDots": false, "groupCells": "80%"}'>
+      <p>RELATED PRODUCTS</p>
+      <div className="main-gallery js-flickity" data-flickity='{"pageDots": false, "cellAlign": "left"}'>
         {relatedProductsInfo.map((relatedProductInfo, index) => {
           return (<ProductCard key={index} relatedProductInfo={relatedProductInfo}/>);
         })}
--- client/src/components/RelatedItems/YourOutfit.jsx
+++ client/src/components/RelatedItems/YourOutfit.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import AddOutfit from './AddOutfit.jsx';
 import ProductCard from './ProductCard.jsx';
+import './relatedItemsStyle.css';
 
 function YourOutfit({outfitProductsInfo}) {
   return (
     <div>
       <p>YOUR OUTFIT</p>
-      <div className="main-gallery js-flickity" data-flickity='{"pageDots": false, "cellAlign": "left", .flickity-button:disabled {display: none}}'>
+      <div className="main-gallery js-flickity" data-flickity='{"pageDots": false, "cellAlign": "left"}'>
         <AddOutfit />
         {outfitProductsInfo.map((outfitProductInfo, index) => {
           return (<ProductCard key={index} outfitProductInfo={outfitProductInfo} />);
