bb0cffbd1b49c3ac7d6552b0ef24a528c718475a
Regina Grogan <regina.r.grogan@gmail.com>
2021-12-30 21:08:00 -0600

    Change AddOutfit to be a bootstrap card and add plus icone as component image source.

--- client/src/components/RelatedItems/AddOutfit.jsx
+++ client/src/components/RelatedItems/AddOutfit.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import addIcone from './images/addIcone.png';
 
 function AddOutfit(props) {
   return (
-    <div>
-      Add to Outfit
-      <button>+</button>
+    <div className="card border-dark" style={{width: '20em', margin: '1em'}}>
+      <img className="card-img-top" src={addIcone} style={{width: 'auto', height: '20vw', objectFit: 'cover'}} alt="Product image"></img>
+      <div className="card-body">
+        <h5 className="card-title">Add to Outfit</h5>
+      </div>
     </div>
-  )
+  );
 }
 
 export default AddOutfit;
