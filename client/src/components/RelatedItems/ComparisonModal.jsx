eff31147c92a0c61d3dcfa69ad8e6b8f9e9a4440
Regina Grogan <regina.r.grogan@gmail.com>
2021-12-31 14:38:27 -0600

    Change ComparisonModal to be a bootstrap modal

--- client/src/components/RelatedItems/ComparisonModal.jsx
+++ client/src/components/RelatedItems/ComparisonModal.jsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 function ComparisonModal(props) {
   return (
-    <div>
-    ComparisonModal
+    <div className="modal-dialog modal-dialog-scrollable">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title">COMPARING</h5>
+          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div className="modal-body">
+          <p>Modal body text goes here.</p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ComparisonModal;
+
