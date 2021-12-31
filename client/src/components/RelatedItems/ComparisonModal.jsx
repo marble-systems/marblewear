4dde9b26ce8cbfc87afa791184866766ee70da76
Regina Grogan <regina.r.grogan@gmail.com>
2021-12-31 16:43:26 -0600

    Change actionButton to toggle ComparisonModal. Change ComparisonModal to render in the center of the page.

--- client/src/components/RelatedItems/ComparisonModal.jsx
+++ client/src/components/RelatedItems/ComparisonModal.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 function ComparisonModal(props) {
   return (
-    <div className="modal-dialog modal-dialog-scrollable">
-      <div className="modal-content">
-        <div className="modal-header">
-          <h5 className="modal-title">COMPARING</h5>
-          <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div className="modal-body">
-          <p>Modal body text goes here.</p>
+    <div className="modal fade" id="comparisonModal" tabIndex="-1"  aria-hidden="true">
+      <div className="modal-dialog modal-dialog-scrollable modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">COMPARING</h5>
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div className="modal-body">
+            <p>Text</p>
+          </div>
         </div>
       </div>
     </div>
