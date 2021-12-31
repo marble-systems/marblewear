a508c3895457f50079672f2a419da12308d01e9c
Aaron Finsrud <aaronfinsrud@gmail.com>
2021-12-31 09:42:51 -0500

    implement ReviewListEntry component (except for "verified purchaser" and 1-click/user requirements

--- client/src/components/Reviews/ReviewList/ReviewListEntry/ReviewListEntry.jsx
+++ client/src/components/Reviews/ReviewList/ReviewListEntry/ReviewListEntry.jsx
@@ -6,6 +6,16 @@ import Modal from '../../../SharedComponents/Modal.jsx';
 const SUMMARY_CHAR_LIMIT = 60;
 const BODY_CHAR_LIMIT = 250;
 const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const formatDate = (input) => {
+  var date = new Date(input);
+  return `${MONTHS[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`;
+};
+const checkmarkIcon = () => {
+  return (<svg height="24px" viewBox="0 0 24 24" width="24px" fill="green">
+    <path d="M0 0h24v24H0z" fill="none" /><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
+  </svg>);
+};
+
 
 class ReviewListEntry extends React.Component {
   constructor(props) {
@@ -22,9 +32,9 @@ class ReviewListEntry extends React.Component {
     this.setState({ expanded });
   }
 
-  handleToggleModalClick (currentImage) {
+  handleToggleModalClick(currentImage) {
     let modalShowing = !this.state.modalShowing;
-    this.setState({currentImage, modalShowing});
+    this.setState({ currentImage, modalShowing });
   }
 
   handleHelpfulClick() {
@@ -42,62 +52,84 @@ class ReviewListEntry extends React.Component {
     let { expanded, modalShowing, currentImage } = this.state;
     let { review } = this.props;
     let { rating, summary, recommend, response, body, date, reviewer_name, helpfulness, photos } = review;
-    date = new Date(date);
+    let modalImage = () => (<img src={currentImage}></img>);
+
     return (
-      <div style={{width: '24em'}}>
+      <div style={{ borderBottom: '1px solid gray', width: '24em' }}>
+
+        {/* STARS USERNAME & DATE */}
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           <Stars rating={rating} />
-          {`${reviewer_name}, `}
-          {`${MONTHS[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`}
+          {`${reviewer_name}, ${formatDate(date)}`}
         </div>
+
+        {/* REVIEW SUMMARY */}
         <div style={{ fontWeight: 'bold' }}>
           {summary.length > SUMMARY_CHAR_LIMIT ? `${summary.substring(0, SUMMARY_CHAR_LIMIT)}...` : summary}
         </div>
+
+        {/* REVIEW BODY */}
         <div>
           {(body.length <= BODY_CHAR_LIMIT || expanded)
             ? body
-            : <span>{body.substring(0, BODY_CHAR_LIMIT)}... <a style={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={this.handleShowMoreClick.bind(this)}>Show more</a></span>}
+            : <span>{body.substring(0, BODY_CHAR_LIMIT)}...
+              <a
+                style={{ textDecoration: 'underline', cursor: 'pointer' }}
+                onClick={this.handleShowMoreClick.bind(this)}>
+                Show more
+              </a>
+            </span>}
         </div>
+
+        {/* RECOMMENDATION */}
         <div>
           {recommend
             ? <div>
-              <svg height="24px" viewBox="0 0 24 24" width="24px" fill="green">
-                <path d="M0 0h24v24H0z" fill="none" /><path d="M9 16.17L4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z" />
-              </svg>
+              {checkmarkIcon()}
               I recommend this product
             </div>
             : ''}
         </div>
+
+        {/* RESPONSE */}
         <div>
           {response ? `Response from seller: ${response}` : ''}
         </div>
-        <div style={{display: 'flex', flexDirection: 'row', flexShrink: 1}}>
+
+        {/* IMAGES */}
+        <div style={{ display: 'flex', flexDirection: 'row', flexShrink: 1 }}>
           {photos.map((url, idx) => {
             return (
-              <div onClick={()=>{this.handleToggleModalClick(url);}} key={idx} style={{ margin: '5px', cursor: 'pointer'}}>
+              <div onClick={() => { this.handleToggleModalClick(url); }} key={idx} style={{ margin: '5px', cursor: 'pointer' }}>
                 <img src={url} width='50px' />
               </div>
             );
           })
           }
         </div>
+
         <Modal
           show={modalShowing}
-          body={currentImage}
+          body={modalImage}
           onClose={this.handleToggleModalClick.bind(this)}/>
-        Helpful? &nbsp;
-        <a style={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={this.handleHelpfulClick.bind(this)}>Yes</a>&nbsp;
-        {`(${helpfulness}) | `}
-        <a style={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={this.handleReportClick.bind(this)}>Report</a>
-        <div>
-        </div>
+
+        {/* HELPFUL? YES REPORT */}
+        <span>
+          Helpful? &nbsp;
+          <a style={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={this.handleHelpfulClick.bind(this)}>
+            Yes &nbsp;
+          </a>
+          {`(${helpfulness}) | `}
+          <a style={{ textDecoration: 'underline', cursor: 'pointer' }} onClick={this.handleReportClick.bind(this)}>
+            Report
+          </a>
+        </span>
       </div>
     );
   }
 }
 
 /* TODO:
- thumbnail -> full res modal
  if user made transaction -> Verified purchaser
  prevent users from clicking buttons more than once
 */
