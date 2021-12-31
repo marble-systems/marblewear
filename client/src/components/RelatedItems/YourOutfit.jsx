47fefc1d31d2b763210434b17b86adde4f93d746
Regina Grogan <regina.r.grogan@gmail.com>
2021-12-30 20:13:44 -0600

    Change YourOutfit component to be a carousel and fix linter errors.

--- client/src/components/RelatedItems/YourOutfit.jsx
+++ client/src/components/RelatedItems/YourOutfit.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import AddOutfit from './AddOutfit.jsx';
 import ProductCard from './ProductCard.jsx';
 
 function YourOutfit({outfitProductsInfo}) {
   return (
     <div>
-      <AddOutfit />
-      {outfitProductsInfo.map(outfitProductInfo => {
-        return <ProductCard outfitProductInfo={outfitProductInfo} />
-      })}
+      <p>YOUR OUTFIT</p>
+      <div className="main-gallery js-flickity" data-flickity='{"pageDots": false, "groupCells": "80%"}'>
+        <AddOutfit />
+        {outfitProductsInfo.map((outfitProductInfo, index) => {
+          return (<ProductCard key={index} outfitProductInfo={outfitProductInfo} />);
+        })}
+      </div>
     </div>
-  )
+  );
 }
+
+YourOutfit.propTypes = {
+  outfitProductsInfo: PropTypes.object
+};
 
 export default YourOutfit;
