37417ca21218fa034a4aa802032d47d8536b2323
Regina Grogan <regina.r.grogan@gmail.com>
2022-01-01 11:26:03 -0600

    Fixed typo in image AddIcon name

--- client/src/components/RelatedItems/AddOutfit.jsx
+++ client/src/components/RelatedItems/AddOutfit.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import addIcone from './images/addIcone.png';
+import addIcon from './images/addIcon.png';
 
 function AddOutfit(props) {
   return (
     <div className="card border-dark" style={{width: '20em', margin: '1em'}}>
-      <img className="card-img-top" src={addIcone} style={{width: 'auto', height: '20vw', objectFit: 'cover'}} alt="Product image"></img>
+      <img className="card-img-top" src={addIcon} style={{width: 'auto', height: '20vw', objectFit: 'cover'}} alt="Product image"></img>
       <div className="card-body">
         <h5 className="card-title">Add to Outfit</h5>
       </div>
