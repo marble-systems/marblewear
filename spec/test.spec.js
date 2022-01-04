37fc023a1b00565defe2ffdc11cd532189fc5c67
Aaron Finsrud <aaronfinsrud@gmail.com>
2022-01-03 19:24:57 -0500

    initialized test.spec.js file

---
+++ spec/test.spec.js
@@ -0,0 +1,32 @@
+/* eslint-disable */
+
+/* References:
+* https://reactjs.org/docs/testing-recipes.html
+* https://jestjs.io/docs/tutorial-react
+*/
+
+describe('ProductDetails', function() {
+  it('Happy path test', function() {
+    //test goes here
+    expect(true).toBe(true);
+  });
+});
+
+describe('QandA', function() {
+  it('Happy path test', function() {
+    expect(true).toBe(true);
+  });
+});
+
+describe('RelatedItems', function() {
+  it('Happy path test', function() {
+    expect(true).toBe(true);
+  });
+});
+
+describe('Reviews', function() {
+  it('Happy path test', function() {
+    expect(true).toBe(true);
+  });
+});
+
