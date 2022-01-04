9d95ca37ee1ee84d493c1e273bc4c96235fce29c
Regina Grogan <regina.r.grogan@gmail.com>
2022-01-03 18:48:15 -0600

    Implement getProductData function that gets product info from API

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,34 +1,59 @@
 import React from 'react';
+import axios from 'axios';
 import Reviews from './Reviews/Reviews.jsx';
 import ProductOverview from './ProductDetails/ProductOverview.jsx';
 import QuestionList from './QandA/QuestionList.jsx';
 import RelatedItems from './RelatedItems/RelatedItems.jsx';
 import NavBar from './Navbar.jsx';
 
-import Reviewsdata from './Reviews/reviewsData.js';
-import QuestionListdata from './QandA/QandAListData.js';
-import relatedItemsdata from './RelatedItems/relatedItemsData.js';
-import productListDummyData from './ProductDetails/sampleData/productListDummyData.js';
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentProductID: '39333',
+      currentProductID: '',
       currentStyleID: 234004,
-      productList: productListDummyData,
+      productList: [],
       reviews: [],
-      questionList: QuestionListdata,
-      relatedItems: relatedItemsdata,
-
+      questionList: [],
+      relatedItems: [],
     };
+    this.cachedProducts = {};
     this.changeCurrentStyle = this.changeCurrentStyle.bind(this);
-
   }
+
+  componentDidMount() {
+    this.getProductData('39333');
+  }
+
+  getProductData(productId) {
+    let currentProduct = this.cachedProducts.productId;
+    if (currentProduct) {
+      let {productList, reviews, questionList} = currentProduct;
+      this.setState({
+        currentProductID: productId,
+        productList,
+        reviews,
+        questionList
+      });
+    } else {
+      axios.get('./products', {
+        params: {
+          productId
+        }
+      })
+        .then(result => {
+          this.cachedProducts.productId = result;
+          this.getProductData(productId);
+        })
+        .catch(err => {
+          alert(`Error ${err} occured`);
+        });
+    }
+  }
+
   changeCurrentStyle(id) {
     this.setState({ currentStyleID: id });
   }
-
 
   render() {
     return (
