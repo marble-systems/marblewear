480e69e56b4935c2443dedba3b79c08d056058bd
Alexander Huerta <alexhuerta305@gmail.com>
2022-01-03 19:44:20 -0500

    implemented a function that will make the next image the new main image  when the current main image is clicked. I wrote but am not using a function with the inverse outcome to use to show the previous pic

--- client/src/components/ProductDetails/ImageGallery.jsx
+++ client/src/components/ProductDetails/ImageGallery.jsx
@@ -12,22 +12,54 @@ function ImageGallery ({productStylesArray, currentStyleID}) {
   const imageGallery = currentStyleObject[0].photos.map((photo)=> photo.thumbnail_url);
 
   const [currentImage, updateMainImage] = useState(imageGallery[0]);
+  //test
+  const [currentMainImageIndex, updateMainImageIndex] = useState(0);
+
 
   const setThumbnailClass = (image) => {
     return image === currentImage ? 'imageThumbnailMain' : 'imageThumbnail';
   };
 
 
+  const showNextImage = ()=>{
+    if(currentMainImageIndex !== imageGallery.length-1) {
+      updateMainImageIndex(currentMainImageIndex+1);
+      const newImg = imageGallery.filter((image)=> imageGallery.indexOf(image) === currentMainImageIndex+1);
+      updateMainImage(newImg);
+    }
+  };
+
+  const showPreviousImage = ()=>{
+    if(currentMainImageIndex > 0) {
+      updateMainImageIndex(currentMainImageIndex-1);
+      const newImg = imageGallery.filter((image)=> imageGallery.indexOf(image) === currentMainImageIndex-1);
+      updateMainImage(newImg);
+    }
+  };
+
+
   return (
     <div clasName="container">
-      <img className="mainImage" src={currentImage}/>
+      <img
+        className="mainImage carousel slide"
+        data-ride="carousel"
+        src={currentImage}
+        onClick= { () => {showNextImage();} }
+      />
+
+
+
       {imageGallery.map((image) =>
         // eslint-disable-next-line react/jsx-key
         <div clasName="row">
           <img
             className={setThumbnailClass(image)}
             src={image}
-            onClick={() => updateMainImage(image)}/>
+            onClick={() => {
+              let indx = imageGallery.indexOf(image);
+              updateMainImageIndex(indx);
+              updateMainImage(image);}
+            }/>
         </div>
       )}
     </div>
